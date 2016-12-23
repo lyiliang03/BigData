8d82cdc2812dfae5d5a3bcd98141954ff49f6eb4
lyiliang03 <395938768@qq.com>
2016-12-23 15:33:11 +0800

    Initial commit

---
+++ src/main/java/com/yu/hadoop/UrlToHdfs.java
@@ -0,0 +1,28 @@
+package com.yu.hadoop;
+
+/**
+ * Created by kevin on 2016/12/23.
+ */
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
+import org.apache.hadoop.io.IOUtils;
+
+public class UrlToHdfs {
+    static{
+        URL.setURLStreamHandlerFactory(new FsUrlStreamHandlerFactory());
+    }
+
+    public static void main(String[] args) throws Exception {
+        String urlStr = "hdfs://master:9000/home/hadoop/hadoop-2.6.5/data/fs/file1.txt" ;
+        URL url = new URL(urlStr);
+        InputStream is = url.openStream();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        IOUtils.copyBytes(is, baos, 1024);
+        IOUtils.closeStream(is);
+        System.out.println(new String(baos.toByteArray()));
+    }
+
+}
